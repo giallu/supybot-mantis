d34d5202b80c631b3a25e5b6e27306035e867768
Gianluca Sforna <giallu@gmail.com>
2008-09-10 12:38:43 +0200

    Add plugins info

--- Mantis/__init__.py
+++ Mantis/__init__.py
@@ -38,17 +38,18 @@ import supybot.world as world
 
 # Use this for the version of this plugin.  You may wish to put a CVS keyword
 # in here if you're keeping the plugin in CVS or some similar system.
-__version__ = ""
+__version__ = "0.1"
 
 # XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.authors.unknown
+__author__ = supybot.Author('Gianluca Sforna', 'giallu',
+                            'giallu@gmail.com')
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = '' # 'http://supybot.com/Members/yourname/Mantis/download'
+__url__ = 'http://github.com/giallu/supybot-mantis'
 
 import config
 import plugin
